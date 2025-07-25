f53d24a538048486e9e2d74ff7df2bfff8176c58
nagaraja konana veeranna <nveeranna@altimetrik.com>
2025-07-25 17:45:27 +0530

    modified the terraform file main.tf for Azure provider

--- app-service-and-sql-database/main.tf
+++ app-service-and-sql-database/main.tf
@@ -1,10 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = "66bc95b7-5c49-4ea6-abcc-38ae21688cf2"
+   features {
+    # Example: Customizing Key Vault behavior
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+
+    # Example: Customizing Virtual Machine behavior
+    virtual_machine_scale_set {
+      force_delete = true
+    }
+  }
+}
 resource "azurerm_resource_group" "RG-Terraform" {
   name     = "terraform-resource-group"
   location = "West Europe"
 }
 
 resource "azurerm_app_service_plan" "ASP-TerraForm" {
-  name                = "terraform-appserviceplan"
+  name                = "nagclk-terraform-appserviceplan"
   location            = azurerm_resource_group.RG-Terraform.location
   resource_group_name = azurerm_resource_group.RG-Terraform.name
 
@@ -15,7 +39,7 @@ resource "azurerm_app_service_plan" "ASP-TerraForm" {
 }
 
 resource "azurerm_app_service" "AS-Terraform" {
-  name                = "app-service-terraform"
+  name                = "nagclk-app-service-terraform"
   location            = azurerm_resource_group.RG-Terraform.location
   resource_group_name = azurerm_resource_group.RG-Terraform.name
   app_service_plan_id = azurerm_app_service_plan.ASP-TerraForm.id
@@ -32,24 +56,27 @@ resource "azurerm_app_service" "AS-Terraform" {
   connection_string {
     name  = "Database"
     type  = "SQLServer"
-    value = "Server=tcp:${azurerm_sql_server.terraform-sqlserver.fully_qualified_domain_name} Database=${azurerm_sql_database.terraform-sqldatabase.name};User ID=${azurerm_sql_server.terraform-sqlserver.administrator_login};Password=${azurerm_sql_server.terraform-sqlserver.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+    value = "Server=tcp:${azurerm_mssql_server.terraform-sqlserver.fully_qualified_domain_name} Database=${azurerm_mssql_database.terraform-sqldatabase.name};User ID=${azurerm_mssql_server.terraform-sqlserver.administrator_login};Password=${azurerm_mssql_server.terraform-sqlserver.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
   }
 }
 
-resource "azurerm_sql_server" "terraform-sqlserver" {
+resource "azurerm_mssql_server" "terraform-sqlserver" {
   name                         = "terraform-sqlserver"
   resource_group_name          = azurerm_resource_group.RG-Terraform.name
   location                     = azurerm_resource_group.RG-Terraform.location
   version                      = "12.0"
-  administrator_login          = "houssem"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = "nagAdmin"
+  administrator_login_password = "Admin@123456#"
 }
 
-resource "azurerm_sql_database" "terraform-sqldatabase" {
-  name                = "terraform-sqldatabase"
-  resource_group_name = azurerm_resource_group.RG-Terraform.name
-  location            = azurerm_resource_group.RG-Terraform.location
-  server_name         = azurerm_sql_server.terraform-sqlserver.name
+resource "azurerm_mssql_database" "terraform-sqldatabase" {
+  name         = "terraform-sqldatabase"
+  server_id    = azurerm_mssql_server.terraform-sqlserver.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
 
   tags = {
     environment = "production"
